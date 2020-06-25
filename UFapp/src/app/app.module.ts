38e8fb1113b4f8291ec9402b090e3ed530dd5e76
Monika Sikora <rascalowa9@gmail.com>
2020-06-25 15:41:45 +0200

    added server component

--- UFapp/src/app/app.module.ts
+++ UFapp/src/app/app.module.ts
@@ -1,12 +1,17 @@
+import { ServerComponent } from './server/server.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  title = 'MyApp';
+}
