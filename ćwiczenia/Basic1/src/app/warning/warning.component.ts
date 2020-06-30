d4febc11bf6d5bfbc718f364d841d1d2ab55dc72
Monika Sikora <rascalowa9@gmail.com>
2020-06-30 12:43:27 +0200

    nested components added

---
+++ ćwiczenia/Basic1/src/app/warning/warning.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning',
+  templateUrl: './warning.component.html',
+  styleUrls: ['./warning.component.css'],
+})
+export class WarningComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
