ae770c1cf51f6b59a0a3c88a32fce0cc279be5b2
Monika Sikora <monikastanko90@gmail.com>
2020-07-29 17:47:24 +0200

    Changes merged, already up to date

--- SectionsExercises/directives-start/src/app/app.component.ts
+++ SectionsExercises/directives-start/src/app/app.component.ts
@@ -6,14 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
   //numbers = [1, 2, 3, 4, 5];
   oddNumbers = [1, 3, 5];
   evenNumbers = [2, 4];
   onlyOdd = false;
   value = 10;
-=======
-  numbers = [1, 2, 3, 4, 5];
-  onlyOdd = false;
->>>>>>> a91fc7526995f4c0bae064159859458534cd7396
 }
