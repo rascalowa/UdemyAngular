184b203bf5d641529a0e2a01c508bb88d8159379
Monika Sikora <monikastanko90@gmail.com>
2020-09-17 11:47:17 +0200

    Recipes section duplicated

--- CourseProject/src/app/app.module.ts
+++ CourseProject/src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -9,26 +8,24 @@ import { RecipeService } from './recipe-book/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AuthComponent } from './auth/auth.component';
 import { RecipesModule } from './recipe-book/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
+    AuthModule,
     SharedModule
   ],
   providers: [
---
+++ CourseProject/src/app/recipes/recipe.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+
+@Injectable()
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Beet green soup',
+  //     'Very tasty soup made from beet leaves',
+  //     'https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Olivka_%28Novosibirsk_cafe%29_03.jpg/640px-Olivka_%28Novosibirsk_cafe%29_03.jpg',
+  //     [
+  //       new Ingredient('Beet Leaves', 3),
+  //       new Ingredient('Sour Cream', 1),
+  //       new Ingredient('Carrot', 2),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Chicken soup',
+  //     'Best soup ever!',
+  //     'https://p0.pikist.com/photos/845/1021/soup-chicken-soup-eat-food-court-cook-vegetables-delicious-noodles.jpg',
+  //     [
+  //       new Ingredient('Chicken Leg', 2),
+  //       new Ingredient('Onion', 1),
+  //       new Ingredient('Carrot', 3),
+  //       new Ingredient('Parsley', 2),
+  //     ]
+  //   ),
+  // ];
+  private recipes: Recipe[] = [];
+
+  constructor(private slService: ShoppingListService) { }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+}
