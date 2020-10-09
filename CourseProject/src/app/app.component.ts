597b5850446df802b7d0b1699e5eb3e4bf4719c3
Monika Sikora <monikastanko90@gmail.com>
2020-10-09 08:42:29 +0200

    Angular Universal (AutoLogin works only in the browser)

--- CourseProject/src/app/app.component.ts
+++ CourseProject/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import * as fromApp from './store/app.reducer';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 
 import * as AuthActions from './auth/store/auth.actions'
 import { LoggingService } from './logging.service';
 import { Store } from '@ngrx/store';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -13,11 +14,17 @@ import { Store } from '@ngrx/store';
 export class AppComponent implements OnInit {
   constructor(
     private store: Store<fromApp.AppState>,
-    private loggingService: LoggingService
+    private loggingService: LoggingService,
+    //we inject hard coded value - no type needed
+    //please look for globally provided value with this identifier, and inject this globl value to this class and store it in this platformId property
+    @Inject(PLATFORM_ID) private platformId
   ) { }
 
   ngOnInit() {
-    this.store.dispatch(new AuthActions.AutoLogin());
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
+
     this.loggingService.printLog('Hello from AppComponent ngOnInit');
   }
 }
--- CourseProject/src/app/app.module.ts
+++ CourseProject/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -19,7 +20,8 @@ import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
     StoreModule.forRoot(fromApp.appReducer),
